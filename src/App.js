82d6368426d08c53010d71667034496c57bccfd6
AtsukiTonomura <tonomura@mfeed.ad.jp>
2021-02-07 20:39:33 +0900

    jsx and js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, { Component } from 'react';
+
+// jsx でのコーディング
 function App() {
   return (
-    <div>
-      <h1>Hello, World!!</h1>
-    </div>
+    <div><h1>Hello, World!!</h1></div>
   );
 }
+
+// // js での コーディング
+// class App extends Component {
+//   render() {
+//     return React.createElement (
+//         "div",
+//         null,
+//         "Hello,world!!!!!!!!!!!!!"
+//       );
+//   }
+// }
 
 export default App;
